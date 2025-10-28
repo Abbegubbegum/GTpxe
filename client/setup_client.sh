5d78c2b06d30c7a796e6dce641274c18092ab51b
Albin Norbäck <albin.nojback@gmail.com>
2025-10-28 14:35:04 +0100

    Add small battery check

--- client/setup_client.sh
+++ client/setup_client.sh
@@ -20,7 +20,7 @@ mkdir -p "$REPO_PATH"
 cd "$REPO_PATH" || exit
 
 apk update
-apk fetch --recursive memtester stress-ng smartmontools nvme-cli util-linux python3
+apk fetch --recursive memtester stress-ng smartmontools nvme-cli util-linux python3 acpi
 apk index -o APKINDEX.tar.gz -- *.apk
 
 lbu add /var/custom-repo/
--- todo.md
+++ todo.md
@@ -1,8 +1,7 @@
 TODO LIST:
 
--   Read Harddrives and maybe check for error flags and logs?
 -   Mouse test ?
 -   Mousepad test ?
 -   Fiber ?
--   Device identification, for unique hardware keyboards
 -   Speaker Test
+-   Batteri
